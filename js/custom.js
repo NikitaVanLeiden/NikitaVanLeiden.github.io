ce18ec10be58ccd1d71d13881206c029a57aebe1
Nikita Van Leiden <110541092+NikitaVanLeiden@users.noreply.github.com>
2022-08-03 17:48:48 +0200

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	
+	/* --------------------------------- Initialize Cufon --------------------------------- */
+	
+	Cufon.replace('h1, h1.quote, h2, h3, h4');
+
+});
+  
